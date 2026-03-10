cf568d9449ea464ac5cfc4cfb9913baed39fa17b
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-10 13:47:42 +0530

    Add files via upload

---
+++ Exp4/activity4.2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:35:04 2026
+
+@author: priyanka
+"""
+
+prices = [250, 120, 75, 300, 90]
+
+total_bill = sum(prices)
+
+print("Total Bill:", total_bill)
