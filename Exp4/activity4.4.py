cb263d2aeeb65377806e717fcb065873d6411bee
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-10 13:48:19 +0530

    Add files via upload

---
+++ Exp4/activity4.4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:40:05 2026
+
+@author: priyanka
+"""
+numbers = [9876543210, 9123456780, 9876543210, 9988776655, 9123456780]
+
+unique_numbers = list(set(numbers))
+
+print("Unique Numbers:", unique_numbers)
