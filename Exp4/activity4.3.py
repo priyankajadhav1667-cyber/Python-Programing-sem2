3afab67fcaa32a48f4c1870e39e2391837ba383c
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-10 13:48:00 +0530

    Add files via upload

---
+++ Exp4/activity4.3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:36:13 2026
+
+@author: priyanka
+"""
+
+feedback = "Great service and fast delivery"
+
+reversed_feedback = feedback[::-1]
+
+print("Reversed Feedback:", reversed_feedback)
