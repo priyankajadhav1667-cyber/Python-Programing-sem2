7c0b34dddc97fe1579e646a5c645c20aa6ad421c
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-10 13:51:48 +0530

    Add files via upload

---
+++ Exp4/act4.5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:49:59 2026
+
+@author: priyanka
+"""
+review = "good product good quality good price"
+word = "good"
+
+count = review.count(word)
+
+print("Word appears:", count, "times")
+
+
+
+
