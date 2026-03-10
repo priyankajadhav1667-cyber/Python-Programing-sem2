8f0ae7750ca24b33a50284fb80ac966a8d665a90
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-10 13:47:25 +0530

    Add files via upload

---
+++ Exp4/activity4.1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 13:32:32 2026
+
+@author: priyanka
+"""
+
+
+marks = [78, 85, 90, 67, 88]
+
+average = sum(marks) / len(marks)
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
