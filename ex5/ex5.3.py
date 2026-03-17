c183d55c4733337170d89feeed0e4b3558fab105
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-17 13:19:48 +0530

    Add files via upload

---
+++ ex5/ex5.3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:53:07 2026
+
+@author: priyanka
+"""
+
+classA = {1, 2, 3, 4, 5}
+classB = {4, 5, 6, 7}
+
+# Common students
+common = classA.intersection(classB)
+
+print(common)
