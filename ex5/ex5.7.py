e8a5a0e33ff845047b6f1029f8a74fe717828f47
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-17 13:23:16 +0530

    Add files via upload

---
+++ ex5/ex5.7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 13:02:16 2026
+
+@author: priyanka
+"""
+account1 = {"rahul", "sneha", "amit", "pooja"}
+account2 = {"amit", "pooja", "karan"}
+
+common_followers = account1 & account2
+
+print(common_followers)
