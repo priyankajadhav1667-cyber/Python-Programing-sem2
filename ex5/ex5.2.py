125d0e1339d803e2d9e72e04c469ec334018c547
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-17 13:17:20 +0530

    Add files via upload

---
+++ ex5/ex5.2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:46:18 2026
+
+@author: priyanka
+"""
+
+books = {"Math": 300, "Physics": 400, "Chemistry": 350}
+
+# Update price
+books["Physics"] = 450
+
+print(books)
