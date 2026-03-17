1f3d6fca644e8a4e51cd9dbb31d06bad90edc35e
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-03-17 13:21:35 +0530

    Add files via upload

---
+++ ex5/ex5.5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 12:57:55 2026
+
+@author: User
+"""
+
+inventory = {"Apples": 50, "Bananas": 30}
+
+# Add new stock
+inventory["Oranges"] = 40
+
+# Update existing stock
+inventory["Apples"] += 20
+
+print(inventory)
