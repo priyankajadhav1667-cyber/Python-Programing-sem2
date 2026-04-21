4b01b6b102bac24fe87c95908ef661b844483f93
priyankajadhav1667-cyber <priyankajadhav1667@gmail.com>
2026-04-21 13:30:52 +0530

    Add files via upload

---
+++ exp8/ex8.3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 12:41:02 2026
+
+@author: priyankatry:
+    total_bill = float(input("Enter total bill: "))
+    people = int(input("Enter number of people: "))
+    share = total_bill / people
+    print("Each person pays:", share)
+except ZeroDivisionError:
+    print("Error: Cannot divide among zero people!")
+"""
+
